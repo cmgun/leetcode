66d802907b92308b995f36749f423084a1ae74c8
cmgun <cm199311@qq.com>
2019-05-15 00:19:36 +0800

    Solution 10: 10. Regular Expression Matching finish

--- src/main/java/com/cmgun/lettcode/dp/Solution10.java
+++ src/main/java/com/cmgun/lettcode/dp/Solution10.java
@@ -9,7 +9,18 @@ package com.cmgun.lettcode.dp;
 public class Solution10 {
 
     public static void main(String[] args) {
-
+        // false
+        System.out.println(isMatch("aaa", "a."));
+        // true
+        System.out.println(isMatch("aaa", "a*"));
+        // false
+        System.out.println(isMatch("aa", "a"));
+        // true
+        System.out.println(isMatch("ab", ".*"));
+        // true
+        System.out.println(isMatch("aab", "c*a*b"));
+        // false
+        System.out.println(isMatch("aab", "b.*"));
     }
 
     /**
@@ -32,15 +43,17 @@ public class Solution10 {
      * borden dp[0]*  dp*[0]:
      * 1. both s and p are empty, dp[][] = true
      * 2. p is empty but s is not empty, dp[][] = false
-     * 3.
+     * 3. s is empty but p is not empty,
+     *   3.1. p not like 'a*', such as 'a.', '*', 'a..', then dp[][] = false
+     *   3.2. p like 'a*', such as 'a*b*', then dp[][] = true
      */
-    public boolean isMatch(String s, String p) {
+    public static boolean isMatch(String s, String p) {
         // initial dp
-        boolean[][] dp = new boolean[s.length()][p.length()];
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
         // both s and p are empty string
         dp[0][0] = true;
         // s is not empty, but p is empty, apparently dp[][] is false
-        for (int i = 0; i <= s.length(); i++) {
+        for (int i = 1; i <= s.length(); i++) {
             dp[i][0] = false;
         }
         // p is not empty, but s is empty
@@ -54,9 +67,19 @@ public class Solution10 {
             }
         }
 
-        // handle normal conditions
+        // normal conditions
         for (int i = 1; i <= s.length(); i++) {
-
+            for (int j = 1; j <= p.length(); j++) {
+                if (p.charAt(j - 1) != '*') {
+                    // s(0->i-1) and p(0->j-1) match result && current p.char is '.' or equals to s.char
+                    dp[i][j] = dp[i -1][j - 1] && (p.charAt(j - 1) == s.charAt(i - 1) || p.charAt(j - 1) == '.');
+                } else {
+                    dp[i][j] = dp[i][j - 2]
+                            || (p.charAt(j - 2) == '.' || p.charAt(j - 2) == s.charAt(i - 1))
+                            && dp[i - 1][j];
+                }
+            }
         }
+        return dp[s.length()][p.length()];
     }
 }
