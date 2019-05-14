8bfcba3ec27ca90ebf076288bf457b68fcb833c8
cmgun <cm199311@qq.com>
2019-05-14 20:21:33 +0800

    10. TODO

--- src/main/java/com/cmgun/lettcode/dp/Solution10.java
+++ src/main/java/com/cmgun/lettcode/dp/Solution10.java
@@ -27,8 +27,31 @@ public class Solution10 {
      *        dp[i][j] = dp[i][j-1]  (preceding element matches one time)
      *        dp[i][j] = dp[i][j-2]  (preceding element matches zero time)
      *
+     * the result of the question is dp[s.length + 1][p.length + 1]
+     *
+     * borden dp[0]*  dp*[0]:
+     * 1. both s and p are empty, dp[][] = true
+     * 2. p is empty but s is not empty, dp[][] = false
+     * 3.
      */
     public boolean isMatch(String s, String p) {
-
+        // initial dp
+        boolean[][] dp = new boolean[s.length()][p.length()];
+        // both s and p are empty string
+        dp[0][0] = true;
+        // s is not empty, but p is empty, apparently dp[][] is false
+        for (int i = 0; i <= s.length(); i++) {
+            dp[i][0] = false;
+        }
+        // p is not empty, but s is empty
+        for (int j = 1; j <= p.length(); j++) {
+            // first char in p couldn't match a empty string
+            if (j == 1) {
+                dp[0][j] = false;
+            } else {
+                // handle *
+                dp[0][j] = p.charAt(j - 1) == '*' && dp[0][j - 2];
+            }
+        }
     }
 }
