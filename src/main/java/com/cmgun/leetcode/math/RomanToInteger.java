2ff28041049141fd0bfc1c9825755ed6e65366d4
cmgun <cm199311@qq.com>
2019-05-21 22:26:51 +0800

    Solution 13: Roman to Integer

--- src/main/java/com/cmgun/leetcode/math/RomanToInteger.java
+++ src/main/java/com/cmgun/leetcode/math/RomanToInteger.java
@@ -23,10 +23,38 @@ public class RomanToInteger {
     }
 
     public static void main(String[] args) {
-
+        // 3
+        System.out.println(romanToInt("III"));
+        // 4
+        System.out.println(romanToInt("IV"));
+        // 9
+        System.out.println(romanToInt("IX"));
+        // 58
+        System.out.println(romanToInt("LVIII"));
+        // 1994
+        System.out.println(romanToInt("MCMXCIV"));
     }
 
+    /**
+     * rules: compare current char with last char
+     * 1. if current char <= last char, result += current char;
+     * 2. if current char > last char, result = result - last char + (current char - last char)
+     *
+     */
     public static int romanToInt(String s) {
-
+        // the range of s is 1 ~ 3999, so s must not be empty.
+        char[] chars = s.toCharArray();
+        int result = charMap.get(chars[0]);
+        int lastNum = result;
+        for (int i = 1; i < chars.length; i++) {
+            int current = charMap.get(chars[i]);
+            if (current > lastNum) {
+                result = result - 2 * lastNum + current;
+            } else {
+                result += current;
+            }
+            lastNum = current;
+        }
+        return result;
     }
 }
