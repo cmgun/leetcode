ce4b407a9c049cf292844c6a49b271e8fa343380
cmgun <cm199311@qq.com>
2019-06-06 09:17:36 +0800

    basic test

--- src/main/java/com/cmgun/other/StaticTest.java
+++ src/main/java/com/cmgun/other/StaticTest.java
@@ -8,12 +8,13 @@ public class StaticTest {
     public static StaticTest single = new StaticTest();
     public static int c = 1;
     public static Integer b;
+//    public static Integer b = 666;
 
     private StaticTest() {
-        System.out.println("----StaticTest()");
+        System.out.println("----StaticTest()-start-" + c);
         c = 2;
         System.out.println("----StaticTest()-" + c);
-//        b = 3;
+        b = 3;
     }
     public static StaticTest getInstance() {
         System.out.println("getInstance()-" + "c:" + c);
@@ -22,11 +23,13 @@ public class StaticTest {
 
     /**
      * 静态对象，静态方法的调用，都不会调用到类的构造方法。
+     * c最开始是0，然后被single赋值为2，跑完之后下一行c再赋值为1
      */
     public static void main(String[] args) {
         System.out.println("main()");
         System.out.println("getInstance: " + getInstance().c);
         System.out.println("single: " + single.c);
+        System.out.println("c:" + c);
         StaticTest dyObj = new StaticTest();
         System.out.println("dyObj: " + dyObj.c);
         System.out.println("end main()");
