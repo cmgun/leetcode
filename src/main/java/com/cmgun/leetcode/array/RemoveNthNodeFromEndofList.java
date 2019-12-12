edf888add66865ce0fcc591b70ba6d2692252416
cmgun <cm199311@qq.com>
2019-12-12 18:25:06 +0800

    update todo

---
+++ src/main/java/com/cmgun/leetcode/array/RemoveNthNodeFromEndofList.java
@@ -0,0 +1,32 @@
+package com.cmgun.leetcode.array;
+
+
+/**
+ * 19. Remove Nth Node From End of List
+ * https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+ *
+ * Created by cmgun on 2019/12/12
+ */
+public class RemoveNthNodeFromEndofList {
+
+    public static void main(String[] args) {
+        //
+    }
+
+    /**
+     * 记录当前节点的前第n个位置
+     *
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+}
