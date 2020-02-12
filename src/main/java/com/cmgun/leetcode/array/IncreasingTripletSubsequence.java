633f0fd3ccc0f0236dbe7b5b3946d38bc4f3bf22
cmgun <cm199311@qq.com>
2020-02-13 00:28:29 +0800

    334

---
+++ src/main/java/com/cmgun/leetcode/array/IncreasingTripletSubsequence.java
@@ -0,0 +1,41 @@
+package com.cmgun.leetcode.array;
+
+
+/**
+ * 334. Increasing Triplet Subsequence
+ * https://leetcode.com/problems/increasing-triplet-subsequence/
+ *
+ * Given an unsorted array return whether an increasing subsequence of length 3 exists or not in the array.
+ *
+ * Formally the function should:
+ *
+ * Return true if there exists i, j, k
+ * such that arr[i] < arr[j] < arr[k] given 0 ≤ i < j < k ≤ n-1 else return false.
+ * Note: Your algorithm should run in O(n) time complexity and O(1) space complexity.
+ *
+ * Example 1:
+ *
+ * Input: [1,2,3,4,5]
+ * Output: true
+ * Example 2:
+ *
+ * Input: [5,4,3,2,1]
+ * Output: false
+ *
+ * Created by cmgun on 2020/2/12
+ */
+public class IncreasingTripletSubsequence {
+
+    /**
+     * Runtime: 1 ms, faster than 99.83% of Java online submissions for Set Matrix Zeroes.
+     * Memory Usage: 42.1 MB, less than 97.14% of Java online submissions for Set Matrix Zeroes.
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+    public boolean increasingTriplet(int[] nums) {
+
+    }
+}
