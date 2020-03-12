7a839f21a4697ee491c0da99dc636dabe4d3742e
cmgun <cm199311@qq.com>
2020-03-13 00:21:18 +0800

    增加Morris Traversal遍历二叉树方式

--- src/main/java/com/cmgun/leetcode/tree/BinaryTreeInorderTraversal.java
+++ src/main/java/com/cmgun/leetcode/tree/BinaryTreeInorderTraversal.java
@@ -31,8 +31,12 @@ public class BinaryTreeInorderTraversal extends BaseTree {
      * @param args
      */
     public static void main(String[] args) {
+        // 传统递归
         TreeNode treeNode1 = initTree(new Integer[]{1,null,2,3});
         System.out.println(inorderTraversal(treeNode1));
+        // Morris Traversal
+        TreeNode treeNode2 = initTree(new Integer[]{1,null,2,3});
+        System.out.println(inorderTraversal1(treeNode2));
     }
 
     /**
@@ -67,6 +71,41 @@ public class BinaryTreeInorderTraversal extends BaseTree {
         result.add(current.val);
         // 右子树遍历
         traversal(result, current.right);
+    }
+
+
+    /**
+     * 使用Morris Traversal
+     * 用线索二叉树进行遍历，步骤：
+     * 1、初始化当前节点 cur 为 root 节点.
+     * 2、如果 cur 没有左孩子，则输出当前节点并将其右孩子作为当前节点,即 cur = cur->right。
+     * 3、如果 cur 有左孩子，在当前节点 cur 的左子树中找到当前节点 cur 在中序遍历下的前驱节点。
+     * a) 如果前驱节点的右孩子为空，将它的右孩子设置为当前节点。当前节点更新为当前节点的左孩子。
+     * b) 如果前驱节点的右孩子为当前节点，将它的右孩子重新设为空（恢复树的形状）。输出当前节点。当前节点更新为当前节点的右孩子。
+     * 4、重复以上2、3直到当前节点为空。
+     *
+     * 时间复杂度O(N)，空间复杂度O(1)；传统递归需要空间复杂度为O(N)
+     */
+    public static List<Integer> inorderTraversal1(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        TreeNode curr = root;
+        TreeNode pre;
+        while (curr != null) {
+            if (curr.left == null) {
+                res.add(curr.val);
+                curr = curr.right; // move to next right node
+            } else { // has a left subtree
+                pre = curr.left;
+                while (pre.right != null) { // find rightmost
+                    pre = pre.right;
+                }
+                pre.right = curr; // put cur after the pre node
+                TreeNode temp = curr; // store cur node
+                curr = curr.left; // move cur to the top of the new tree
+                temp.left = null; // original cur left be null, avoid infinite loops
+            }
+        }
+        return res;
     }
 
 }
